d358af0729300fc5e5f682175928db7c99542f27
dhananjaythomble <dhananjaythomble@gmail.com>
2023-07-04 12:58:04 +0530

    add required validation for login route
    a. username to be alphanumeric
    b. username to be between 6-12 letters
    c. password to allow alphabet, numbers, special characters
    d. minimum password length is 6

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const loginRoute = require("./route/loginRoute");
 
 const port = process.env.PORT || 8080;
+
+app.use(express.json());  // for parsing application/json
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/login", loginRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
